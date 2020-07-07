3bf1407d2795482cf12a79d303da81c56e2594c7
David Colatti <davidcolatti@gmail.com>
2020-07-07 09:37:53 -0400

    feat: not a good package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import ReactDisqusComments from "react-disqus-comments";
 
-function App() {
-  return <div className="App">App</div>;
-}
+const App = () => {
+  function handleNewComment(comment) {
+    console.log(comment);
+  }
+
+  return (
+    <ReactDisqusComments
+      shortname="example"
+      identified="something-unique-12345"
+      title="Example Thread"
+      url="http://localhost:3000/"
+      category_id="123456"
+      onNewComment={handleNewComment}
+    />
+  );
+};
 
 export default App;
